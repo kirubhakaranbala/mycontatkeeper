ebd914285a8bf078f58533df7031f01006849435
kirubala <kirubala@KIRUBALA-IN.oradev.oraclecorp.com>
2020-02-24 20:17:39 +0530

    Express server setup with intia server routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app=express();
+
+//connect database
+connectDB();
+
+ app.get('/',(req,res) => res.json({msg:'welcome to contact keeper api'}));
+
+ app.use('/api/users',require('./routes/users'));
+ app.use('/api/auth',require('./routes/auth'));
+ app.use('/api/contacts',require('./routes/contacts'));
+const PORT=process.env.POST || 5000;
+
+app.listen(PORT ,()=> console.log(`Server started on port ${PORT}`));
