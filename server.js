ec36aa595ad0a866654e8bfb374aa4f45a8293de
kirubala <kirubala@KIRUBALA-IN.oradev.oraclecorp.com>
2020-05-11 10:53:40 +0530

    db issue

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const app=express();
 
-//connect database
+const app = express();
+
+// Connect Database
 connectDB();
 
- app.get('/',(req,res) => res.json({msg:'welcome to contact keeper api'}));
+// Init Middleware
+app.use(express.json({ extended: false }));
 
- app.use('/api/users',require('./routes/users'));
- app.use('/api/auth',require('./routes/auth'));
- app.use('/api/contacts',require('./routes/contacts'));
-const PORT=process.env.POST || 5000;
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-app.listen(PORT ,()=> console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
